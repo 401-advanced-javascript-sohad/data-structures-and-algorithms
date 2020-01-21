816151c417b164990206c626f63f863986f1b622
sohadQtaitat <qtaitat3@gmail.com>
2020-01-21 06:05:24 +0200

    challenge 2

---
+++ challenges/arrayReverse/array Shift/array-shift.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const array = [1,2,3,4,5,6,7];
+const element = 8;
+
+const arrShift = (array, element) => {
+
+  for(i = 0; i < array.length - 1; i++) {
+
+    if(array[index] > array.length / 2) {
+
+      let middleOfArray = array[index];
+
+      console.log('Middle Array:', middleOfArray);
+      let arrs = [...array, ...middleElement]
+      console.log('start: ', arrs);
+      break;
+    };
+  };
+};
+//not finish
+// console.log('Call: ', arrShift(array,element));
+
+module.exports = (insertShuftArray);
